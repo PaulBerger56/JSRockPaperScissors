c315dd3352bcf67a09941f20425c0b89372a89f3
Paul Berger <paulberger56@gmail.com>
2023-06-09 17:36:49 -0400

    Add a function to show the score and one to give an end game message

--- RPS.js
+++ RPS.js
@@ -1,38 +1,61 @@
+let pScore = 0;
+let cScore = 0;
+
+
 document.addEventListener('DOMContentLoaded', function() {
+
+    
+    
     function getComputerChoice() {
         const choices = ['Rock', 'Paper', 'Scissors'];
         let choice = choices[Math.floor(Math.random() * 3)];
         return choice;
     }
 
-    function playARound(playerSelection, computerSelection) {
+    function playARound(playerSelection, computerSelection) {        
+
         let pSelection = playerSelection.toLowerCase();
         let cSelection = computerSelection.toLowerCase();
 
+        
         if (pSelection.localeCompare(cSelection) === 0) {
             return "Draw! Both selected " + computerSelection;
         } else if (pSelection === 'rock') {
             if (cSelection === 'paper') {
-                return 'You Lose! Paper beats Rock';
+                cScore++;
+                updateScore()
+                return 'You Lose! Paper beats Rock';                
             } else {
-                return 'You Win! Rock beats Scissors';
+                pScore++;
+                updateScore()
+                return 'You Win! Rock beats Scissors';                
             }
         } else if (pSelection === 'paper') {
             if (cSelection === 'scissors') {
-                return 'You Lose! Scissors beat Paper';
+                cScore++;
+                updateScore()
+                return 'You Lose! Scissors beat Paper';                
             } else {
-                return 'You Win! Paper beats Rock';
+                pScore++;
+                updateScore()
+                return 'You Win! Paper beats Rock';                
             }
         } else if (pSelection === 'scissors') {
             if (cSelection === 'rock') {
-                return 'You Lose! Rock beats Scissors';
+                cScore++;
+                updateScore()
+                return 'You Lose! Rock beats Scissors';                
             } else {
-                return 'You Win! Scissors beat Rock';
+                pScore++;
+                updateScore()
+                return 'You Win! Scissors beat Rock';                
             }
         } else {
             return 'Sorry, invalid selection';
         }
     }
+
+    
 
     const rock = document.querySelector('.Rock');
     const paper = document.querySelector('.Paper');
@@ -44,20 +67,48 @@ document.addEventListener('DOMContentLoaded', function() {
     result.classList.add('result');
     rpsContainer.appendChild(result);
 
+    const playerScore = document.querySelector("#playerScore");
+    const computerScore = document.querySelector("#cpuScore");
+
+   
     
 
     rock.addEventListener('click', function() {
         result.textContent = playARound('rock', getComputerChoice());
-        console.log(playARound('rock', getComputerChoice()));
+        
     });
 
     paper.addEventListener('click', function() {
         result.textContent = playARound('paper', getComputerChoice());
-        console.log(playARound('paper', getComputerChoice()));
+        
     });
 
     scissors.addEventListener('click', function() {
         result.textContent = playARound('scissors', getComputerChoice());
-        console.log(playARound('scissors', getComputerChoice()));
+        
     });
+
+    function updateScore() {
+        checkGameEnd();
+        playerScore.innerHTML = 'Player Score: ' + pScore;
+        computerScore.innerHTML = 'Computer Score: ' + cScore;
+    }
+
+    function checkGameEnd() {
+        const end = document.querySelector('#endContainer');
+        
+        if(pScore === 5) {
+            const pWin = document.createElement('h2');
+            pWin.classList.add('pWin');
+            pWin.textContent = 'Congratulations! You Beat The Computer!'
+            end.appendChild(pWin);
+        }
+        if(cScore === 5) {
+            const cWin = document.createElement('h2');
+            cWin.classList.add('cWin');
+            cWin.textContent = 'Sorry! You Lost To The Computer!'
+            end.appendChild(cWin);
+        }
+    }
+    
 });
